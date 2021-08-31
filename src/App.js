e21a1ebd5361785737b58933511db0a2498978c6
cmdfas <cmdfas@gmail.com>
2021-08-31 19:25:30 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import config from './aws-exports'
+
+import { API } from 'aws-amplify'
+import { listBlogs } from './graphql/queries'
+import { createBlog } from './graphql/mutations'
+import { onCreateBlog } from './graphql/subscriptions'
+import { useEffect } from 'react'
 
 function App() {
+  // 创建数据
+  const createData = async () => {
+    const blogName = prompt('请输入博客名称')
+    let data = await fetch(config.aws_appsync_graphqlEndpoint, {
+      method: 'POST',
+      headers: {
+        'X-Api-Key': config.aws_appsync_apiKey
+      },
+      body: JSON.stringify({
+        query: `mutation CreateBlog {
+          createBlog(input: {name: "${blogName}"}) {
+            id
+            name
+          }
+        }`,
+      })
+    })
+    data = await data.json()
+    console.log(data)
+  }
+
+  // 获取数据列表
+  const pullData = async () => {
+    let data = await fetch(config.aws_appsync_graphqlEndpoint, {
+      method: 'POST',
+      headers: {
+        'X-Api-Key': config.aws_appsync_apiKey
+      },
+      body: JSON.stringify({
+        query: `query MyQuery {
+          listBlogs {
+            items {
+              name
+              posts {
+                items {
+                  id
+                  title
+                }
+              }
+            }
+          }
+        }`
+      })
+    })
+    data = await data.json()
+    console.log(data)
+  }
+
+  // aws 创建博客
+  const awsCreateBlog = async () => {
+    const blogName = prompt('请输入博客名称')
+    const data = await API.graphql({ query: createBlog, variables: { input: { name: blogName }} })
+    console.log(data)
+  }
+
+  // aws 获取博客列表
+  const awsListBlog = async () => {
+    const data = await API.graphql({ query: listBlogs })
+    console.log(data)
+  }
+
+  useEffect(() => {
+    // 监听博客创建事件
+    const subscription = API.graphql({
+      query: onCreateBlog
+    }).subscribe({
+      next: data => {
+        awsListBlog()
+      },
+      error: err => {
+        console.log(err)
+      }
+    })
+
+    return () => subscription.unsubscribe()
+  }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={createData}>创建博客</button>
+      <button onClick={pullData}>博客列表</button>
+      <br/><br/>
+      <button onClick={awsCreateBlog}>AWS 创建博客</button>
     </div>
   );
 }
